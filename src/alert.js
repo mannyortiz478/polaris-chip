5149e1aa5a25a06a545b109c9b94963902c7d18d
Manny Ortiz <48871686+mannyortiz478@users.noreply.github.com>
2024-02-28 17:17:43 -0500

    new draft changes to alert.js

--- src/alert.js
+++ src/alert.js
@@ -16,6 +16,7 @@ export class MyAlert extends LitElement {
         this.month = "November";
         this.day = 9;
         this.year = 2024;
+        this.isOpen = false;
     }
 
     static get styles() {
@@ -39,6 +40,19 @@ export class MyAlert extends LitElement {
     padding: 1px;
     text-align: center;
 }
+
+.hidden-alert-container {
+    background-color: #ffd100;
+    width: auto;
+    border: black;
+    display: flex;
+    justify-content: center;
+    border: 1px solid;
+    border-radius: 12px;
+    margin: auto;
+    padding: 1px;
+    text-align: center;
+}
 .date {
     text-align: left;
     color: white;
@@ -47,6 +61,12 @@ export class MyAlert extends LitElement {
 .minimize-alert {
     text-align: right;
     margin: 5px;
+    width: auto;
+}
+
+.maximize-alert {
+    text-align: center;
+    margin-top: 10px;
     width: auto;
 }
 .close-btn  {
@@ -61,12 +81,25 @@ export class MyAlert extends LitElement {
     //letter-spacing: 0.03rem;
     text-align: right
 }
+.view-btn {
+    background-color: transparent;
+    color: white;
+    border: none;
+    cursor: pointer;
+    font-family: inherit;
+    font-size: 1rem;
+    line-height: 1.125rem;
+    font-weight: 700;
+    //letter-spacing: 0.03rem;
+    text-align: center;
+    display: inline;  
+}
 .message-wrap {
     padding: 15px;
     height: 100%;
     width: 75%;
 }
-.alert-message {
+.alert-message { 
     color: black;
     font-size: 1.125rem;
     font-style: italic;
@@ -74,6 +107,16 @@ export class MyAlert extends LitElement {
     display: inline;
     font-weight: 700;
     letter-spacing: 0.03rem;
+}
+
+.hidden-message, .small-message {
+    color: black;
+    font-size: 2rem;
+    font-style: italic;
+    line-height: 1.25rem;
+    display: inline;
+    font-weight: 700;
+    letter-spacing: 0.03rem; 
 }
 #sticky {
     position: fixed;
@@ -96,16 +139,38 @@ export class MyAlert extends LitElement {
             this.fancy = false;
         }
     }
-    hideAlert() {
-        const alertContainer = this.shadowRoot.getElementById('sticky');
-        alertContainer.style.display = 'none';
+    hiddenAlert() {
+        this.isOpen = false;
     }
 
-    render() {
+    openAlert() {
+        this.isOpen = true;
+    }
+
+    hiddenAlertContent() {
+        return html`
+        <div class="hidden-alert-container" id="sticky">
+            <div class="date">
+                <h3>${this.month} ${this.day}, ${this.year}</h3>
+            </div>
+            <div class="message-wrap">
+                <div class="small-message">
+                <svg height=50px width=65px xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>alert-circle-outline</title><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z" /></svg>          
+                    <p class="hidden-message">There is an alert!!</p>
+                </div>
+                <div class="maximize-alert">
+                <span @click="${this.openAlert}">
+                    <button class="view-btn">View</button>
+                </span>
+            </div> 
+            </div>
+        </div>
+        `
+    }
+
+    openAlertContent() {
         return html`
         <div class="alert-container" id="sticky">
-            <div class-mid></div>
-        
             <div class="date">
                 <h3>${this.month} ${this.day}, ${this.year}</h3>
             </div>
@@ -116,21 +181,33 @@ export class MyAlert extends LitElement {
                 </div>
             </div>
             <div class="minimize-alert">
-                <span @click="${this.hideAlert}">
+                <span @click="${this.hiddenAlert}">
                     <button class="close-btn">X Close</button>
                 </span>
             </div> 
 
         </div>
-  `;
+        `
+    }
+
+    render() {
+        if (this.isOpen) {
+            return this.openAlertContent();
+        } else {
+            return this.hiddenAlertContent();
+        }
     }
 
     static get properties() {
         return {
+            isOpen: { type: String },
             message: { type: String },
             month: { type: String },
             day: { type: Number },
             year: { type: Number },
+            alertColor: { type: String },
+            sticky: { type: String },
+            status: { tyep: Boolean },
             fancy: { type: Boolean, reflect: true },
         };
     }
